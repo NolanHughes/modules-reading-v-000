2091ded47817dc15fd006483468436c96be4f516
Nolan Hughes <nhughes987@gmail.com>
2017-11-16 18:59:01 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,6 +1,5 @@
 module FancyDance
   module InstanceMethods
-
     def twirl
       "I'm twirling!"
     end
@@ -16,6 +15,7 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
+    
   end
 
   module ClassMethods
