2735a2df2c6eff51f5c8e9a8388c1bd03348e397
Nolan Hughes <nhughes987@gmail.com>
2017-11-16 19:08:13 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,13 +3,13 @@ require_relative './dance_module.rb'
 
 class Dancer
 
-  include FancyDance::InstanceMethods
-  extend FancyDance::ClassMethods
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  # include FancyDance::InstanceMethods
+  # extend FancyDance::ClassMethods
+  # 
+  # attr_accessor :name
+  # 
+  # def initialize(name)
+  #   @name = name
+  # end
 
 end
